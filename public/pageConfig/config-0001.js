d3e6c880c31f078b95c32fddf38c2614a7bf5ebc
adminV <18520504@QQ.com>
2019-11-06 02:55:56 +0800

    配置开始

--- public/pageConfig/config-0001.js
+++ public/pageConfig/config-0001.js
@@ -1,12 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 var __pageConfig__ = {
   "pageName": "测试页面",
   "author": "文强",
   "PM": "文强",
   "createData": "20191109",
-  "baseConfig": {
-    "emptyShopId": true,
-    "query": {},
-    "originConfig": {}
-  },
   "pageConfig": {}
 };
